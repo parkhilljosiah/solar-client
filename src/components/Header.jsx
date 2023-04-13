6a1fa5b60e8b545b3a89bfc9b0938ed9729655ac
Josiah Parkhill <parkhilljosiah@gmail.com>
2023-04-13 19:34:05 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../assets/stylesheets/Header.css";
+
+export default function Header() {
+  return (
+    <div className="solar__header__container">
+      <div className="solar__header">
+        s<i className="bi bi-sun-fill"></i>lar
+      </div>
+    </div>
+  );
+}
